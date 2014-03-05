3ac80e1b0b5341e6af4c1a4172b3af152a2b677a
rigid1980 <bin.mengbin@gmail.com>
2014-03-05 23:20:29 +0800

    add standard template to header.

--- io/read_off.m
+++ io/read_off.m
@@ -4,14 +4,18 @@
 %% Syntax
 %   [face,vertex,color] = read_off(filename);
 %% Description
+%   filename specify the file to read.
 %   'vertex' is a 'vert_number x 3' array specifying the position of the vertices.
 %   'face' is a 'face_number x 3' array specifying the connectivity of the mesh.
+%  'color' is a 'vert_number x 3 or face_number x 3' array specifying the color of the vertices or faces.
 
 %%   Example
 %   [face,vertex,color] = read_off('2_2.off');
 
-%   Copyright 2014 Computational Geometry Group,  Mathematics Dept., CUHK
-%   http://www.lokminglui.com/
+%% Copyright 2014 Computational Geometry Group,  Mathematics Dept., CUHK
+%  Website:  http://www.lokminglui.com/
+%  Author: Meng Bin
+%  History:  2014/03/05 file created
 
 function [face,vertex,color] = read_off(filename)
 
