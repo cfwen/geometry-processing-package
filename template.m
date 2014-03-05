cb9e26d05a50aa0d5ef62225ea90a9cf36e27fc9
rigid1980 <bin.mengbin@gmail.com>
2014-03-05 23:20:55 +0800

    standard template

---
+++ template.m
@@ -0,0 +1,18 @@
+%% function_name 
+% description of the function
+
+%% Syntax
+%   [vertex,face] = read_obj(filename);
+%% Description
+%   filename specify the file to read
+%  'vertex' is a 'vert_number x 3' array specifying the position of the vertices.
+%  'face' is a 'face_number x 3' array specifying the connectivity of the mesh.
+
+%%   Example
+%   
+%
+
+%% Copyright 2014 Computational Geometry Group,  Mathematics Dept., CUHK
+%  Website:  http://www.lokminglui.com/
+%  Author: Author Name
+%  History:  yyyy/mm/dd file created
