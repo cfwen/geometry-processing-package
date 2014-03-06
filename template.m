f50b0dc1d2159d296f24196585d5995387ddf6a0
dijkbgf <dijkbgf@gmail.com>
2014-03-06 16:16:42 +0800

    update template to get better published page

--- template.m
+++ template.m
@@ -1,18 +1,22 @@
-%% function_name 
+%% FUNCTION_NAME 
 % description of the function
 
 %% Syntax
-%   [vertex,face] = read_obj(filename);
-%% Description
-%   filename specify the file to read
-%  'vertex' is a 'vert_number x 3' array specifying the position of the vertices.
-%  'face' is a 'face_number x 3' array specifying the connectivity of the mesh.
+% [] = function();
 
-%%   Example
+%% Description
+% 
+
+%% Example
 %   
 %
 
-%% Copyright 2014 Computational Geometry Group,  Mathematics Dept., CUHK
-%  Website:  http://www.lokminglui.com/
-%  Author: Author Name
-%  History:  yyyy/mm/dd file created
+%% Contribution
+% 
+%  Author:
+%  Created: yyyy/mm/dd
+%  Revised: yyyy/mm/dd by [who], [what is revised]
+% 
+%  Copyright 2014 Computational Geometry Group
+%  Department of Mathematics, CUHK
+%  http://www.lokminglui.com
