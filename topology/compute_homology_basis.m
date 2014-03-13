7db0df9491385f65ebdb5e7af316f917d7cab890
C Wen <whywe@live.com>
2014-03-13 13:19:25 +0800

    add topology functions
    add:
    compute_homology_basis
    update:
    cut_graph

---
+++ topology/compute_homology_basis.m
@@ -0,0 +1,33 @@
+function hb = compute_homology_basis(face,vertex)
+
+ee = cut_graph(face,vertex);
+nv = size(vertex,1);
+G = sparse(ee(:,1),ee(:,2),ones(size(ee,1),1),nv,nv);
+G = G+G';
+% v = ee(1);
+[tree,pred] = graphminspantree(G,'METHOD','Kruskal');
+v = find(pred==0);
+[I,J,~] = find(tree+tree');
+eh = setdiff(ee,[I,J],'rows');
+hb = cell(size(eh,1),1);
+for i = 1:size(eh,1)
+    p1 = trace_path(pred,eh(i,1),v);
+    p2 = trace_path(pred,eh(i,2),v);
+    loop = [flipud(p1);eh(i,1);eh(i,2);p2];
+    hb{i} = prune_path(loop);
+end
+
+function path = trace_path(pred,v,root)
+path = [];
+while true
+    path = [path;pred(v)];
+    v = pred(v);
+    if v == root
+        break;
+    end
+end
+
+function path_new = prune_path(path)
+ind = path ~= flipud(path);
+i = find(ind,1)-1;
+path_new = path(i:end-i+1);
--- topology/cut_graph.m
+++ topology/cut_graph.m
@@ -1,21 +1,32 @@
 function ee = cut_graph(face,vertex)
 
 [amf,dual_vertex] = compute_dual_graph(face,vertex);
+[edge,eif] = compute_edge(face);
 [I,J,~] = find(amf);
-el = sqrt(dot(dual_vertex(I,:)-dual_vertex(J,:),dual_vertex(I,:)-dual_vertex(J,:),2));
-[tree, pred] = graphminspantree(amf,'METHOD','Kruskal','Weights',el);
+
+% edge length of original mesh as weight
+el = zeros(size(I));
+for i=1:length(I)
+    ei = face_intersect(face(I(i),:),face(J(i),:));
+    el(i) = norm(vertex(ei(1),:)-vertex(ei(2),:));
+end
+% tree = graphminspantree(amf,'METHOD','Prim','Weights',max(el)-el);
+
+% edge length of dual mesh as weight
+dual_el = sqrt(dot(dual_vertex(I,:)-dual_vertex(J,:),dual_vertex(I,:)-dual_vertex(J,:),2));
+tree = graphminspantree(amf,'METHOD','Prim','Weights',(max(el)-el));
+
 tree = tree+tree';
 [I,J,~] = find(tree);
-[edge,eif] = compute_edge(face);
 [~,ia] = setdiff(eif,[I,J],'rows');
 de = edge(ia,:);
 nv = size(vertex,1);
 G = sparse(de(:,1),de(:,2),ones(size(de,1),1),nv,nv);
 
-% prune the cut graph
+% prune the graph cut
 while true
-    Gs = full(sum(G,2))+full(sum(G,1))';    
-    ind =  Gs == 1;
+    Gs = full(sum(G,2))+full(sum(G,1))';
+    ind = (Gs == 1);
     if sum(ind) ==0
         break;
     end
@@ -25,3 +36,12 @@ end
 
 [I,J,~] = find(G);
 ee = [I,J];
+
+function e = face_intersect(f1,f2)
+b = f1==f2(1);
+e1 = f1(b);
+b = f1==f2(2);
+e2 = f1(b);
+b = f1==f2(3);
+e3 = f1(b);
+e = [e1,e2,e3];
