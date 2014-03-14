0c43aab18e680653c40f367f85d674c079991f6c
C Wen <whywe@live.com>
2014-03-14 13:04:33 +0800

    add graphic functions
    add:
    plot_mesh
    plot_path
    
    update:
    cut_graph2

--- test.m
+++ test.m
@@ -1,20 +1,28 @@
+ee = cut_graph(face,vertex);
+nv = size(vertex,1);
 G = sparse(ee(:,1),ee(:,2),ones(size(ee,1),1),nv,nv);
 Gs = full(sum(G,2))+full(sum(G,1))';
-i = find(Gs==3);
-bd = [];
+branch = find(Gs>2);
+nb = size(branch,1);
 G2 = G+G';
-vertex_new = [vertex;vertex(Gs==2,:);vertex(Gs==3,:);vertex(Gs==3,:)];
+% vertex_new = [vertex;vertex(Gs==2,:);vertex(Gs==3,:);vertex(Gs==3,:)];
 %%
+se = cell(nb,1);
+i = branch(1);
+si = i;
+k = 1;
 while true
-    j = find(G(i,:)==2,1);
-    if isempty(j) 
-        j = find(G(:,i)==1,1);
-        if isempty(j)
-            break;
-        end
+    j = find(G2(i,:),1);
+    if isempty(j)
+        break;
     end
-    bd = [bd;i,j];
-    G(i,j) = G(i,j) - 1;
-    G(j,i) = G(j,i) - 1;
+    si = [si;j];
+    G2(i,j) = 0;
+    G2(j,i) = 0;
+    if Gs(j) > 2
+        se{k} = si;
+        si = j;
+        k = k+1;
+    end
     i = j;
 end
