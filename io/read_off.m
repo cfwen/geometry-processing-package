8d515954a5f5a07a733cb748b7c8e7851ef1a55c
C Wen <cfwen@users.noreply.github.com>
2014-03-14 15:19:22 +0800

    modify doc format
    modify doc format in read_off.m

--- io/read_off.m
+++ io/read_off.m
@@ -1,22 +1,26 @@
-%% read_off 
-% Read mesh data from OFF file
-
+%% read_off
+%  Read mesh data from OFF file
+% 
 %% Syntax
+%   [face,vertex] = read_off(filename)
 %   [face,vertex,color] = read_off(filename)
-
+%
 %% Description
-%   filename specify the file to read.
-%   'vertex' is a 'vert_number x 3' array specifying the position of the vertices.
-%   'face' is a 'face_number x 3' array specifying the connectivity of the mesh.
-%  'color' is a 'vert_number x 3 or face_number x 3' array specifying the color of the vertices or faces.
-
-%%   Example
-%   [face,vertex,color] = read_off('2_2.off');
-
+%  filename : string, file to read.
+%
+%  face   : double array, nf x 3 array specifying the connectivity of the mesh.
+%  vertex : double array, nv x 3 array specifying the position of the vertices.
+%  color  : double array, nv x 3 or nf x 3 array specifying the color of the vertices or faces.
+%
+%% Example
+%   [face,vertex] = read_off('torus.off');
+%   [face,vertex,color] = read_off('torus.off');
+%
 %% Contribution
 %  Author: Meng Bin
-%  History:  2014/03/05 file created
-%  Revised: 2014/03/07 by Meng Bin, Block write to enhance writing speed
+%  History: 2014/03/05 file created
+%  Revised: 2014/03/07 by Meng Bin, block write to enhance writing speed
+%  Revised: 2014/03/14 by Wen, modify doc and code format
 % 
 %  Copyright 2014 Computational Geometry Group
 %  Department of Mathematics, CUHK
@@ -27,42 +31,39 @@ function [face,vertex,color] = read_off(filename)
 fid = fopen(filename,'r');
 if( fid==-1 )
     error('Can''t open the file.');
-    return;
 end
 
 % read header
 [tline] = skip_comment_blank_line(fid,0);
-if ~strcmp(upper(tline(1:3)), 'OFF')
+if ~strcmpi(tline(1:3), 'OFF')
     error('The file is not a valid OFF one.');    
 end
 
-%read number of verteics and faces
+% read number of verteics and faces
 [tline] = skip_comment_blank_line(fid,0);
 [a,tline] = strtok(tline); nvert = str2num(a);
 [a,tline] = strtok(tline); nface = str2num(a);
 
 color = [];
 
-%read vertex info
+% read vertex info
 tot_cnt = 0;
-cols = 0;
 A = [];
-
 tline = '';
 while (~feof(fid) && (isempty(tline) || tline(1) == '#'))
-	pos = ftell(fid);
-	tline = strtrim(fgets(fid));
+    pos = ftell(fid);
+    tline = strtrim(fgets(fid));
 end
 C = regexp(tline,'\s+','split');
 % read columns of vertex line
 cols = size(C,2);
-%rewind to starting of the line 
+% rewind to starting of the line 
 fseek(fid, pos,-1);
-%vertex and color line format string
+% vertex and color line format string
 format = strcat(repmat('%f ', [1, cols]), '\n');
     
-%start reading	
-while (~feof(fid) & tot_cnt < cols*nvert)
+% start reading vertex
+while (~feof(fid) && tot_cnt < cols*nvert)
     [A_,cnt] = fscanf(fid,format, cols*nvert-tot_cnt);
     tot_cnt = tot_cnt + cnt;
     A = [A;A_];
@@ -70,56 +71,56 @@ while (~feof(fid) & tot_cnt < cols*nvert)
 end
 
 if tot_cnt~=cols*nvert
-    warning('Problem in reading vertices. number of vertices doesnt match header.');
+    warning('Problem in reading vertices. number of vertices does not match header.');
 end
 A = reshape(A, cols, tot_cnt/cols);
 vertex = A(1:3,:)';
 
-% extract vertex color	
+% extract vertex color    
 if cols == 6
-	color = A(4:6,:)';
+    color = A(4:6,:)';
 elseif cols > 6
-	color = A(4:7,:)';
+    color = A(4:7,:)';
 end
 
-%read face info
+% read face info
 tot_cnt = 0;
 A = [];
 tline = '';
 while (~feof(fid) && (isempty(tline) || tline(1) == '#'))
-	pos = ftell(fid);
-	tline = strtrim(fgets(fid));
+    pos = ftell(fid);
+    tline = strtrim(fgets(fid));
 end
 C = regexp(tline,'\s+','split');
 % read columns of face line
 nvert_f = str2num(C{1});
 cols = nvert_f+1;
 if isempty(color)
-	cols = size(C,2);
+    cols = size(C,2);
 end
-%rewind to starting of the line 
+% rewind to starting of the line 
 fseek(fid, pos,-1);
-%face and color line format string
+% face and color line format string
 format = strcat(repmat('%d ', [1, nvert_f+1]), repmat('%f ', [1, cols-nvert_f-1]));
 format = strcat(format, '\n');
 
-%start reading		
-while (~feof(fid) & tot_cnt < cols*nface)
+% start reading face
+while (~feof(fid) && tot_cnt < cols*nface)
     [A_,cnt] = fscanf(fid,format, cols*nface-tot_cnt);
     tot_cnt = tot_cnt + cnt;
     A = [A;A_];
-	skip_comment_blank_line(fid,1);
+    skip_comment_blank_line(fid,1);
 end
    
 if tot_cnt~=cols*nface
-    warning('Problem in reading faces. Number of faces doesnt match header.');
+    error('Problem in reading faces. Number of faces does not match header.');
 end
 A = reshape(A, cols, tot_cnt/cols);
 face = A(2:nvert_f+1,:)'+1;
 
 % extract face color
 if cols > nvert_f+1
-	color = A(nvert_f+2:cols,:)';
+    color = A(nvert_f+2:cols,:)';
 end
 
 fclose(fid);
@@ -140,6 +141,5 @@ while (~feof(fid) && (isempty(tline) || tline(1) == '#'))
     tline = strtrim(fgets(fid));
 end
 if rewind==1
-        fseek(fid, pos,-1);
+    fseek(fid, pos,-1);
 end
-
