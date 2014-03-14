2ba9e6ed8917f55ab19cb6ac01c6ea95fe9f38d4
rigid1980 <bin.mengbin@gmail.com>
2014-03-14 11:34:57 +0800

    tentation for slice mesh

--- topology/slice_mesh.m
+++ topology/slice_mesh.m
@@ -1,65 +1,41 @@
-function newTR = slice_mesh(face,ee)
+function [face_s, vertex_s] = slice_mesh(face,vertex,edge,hec,heifc)
+% hec, heifc  denotes half edge of loop
 
-%{
-**************************************************************
-function newTR = sliceMesh(TR,boundary)
+nv = size(vertex,1);
+ne = size(edge,1);
+nf = size(face,1);
 
-To slice the closed mesh along a loop 
+[he,heif] = compute_halfedge(face);
 
-input: 
-TR: triangulation of the surface
-boundary: loop in form of cell
-
-output:
-newTR: triangulation of the sliced surface
-*************************************************************
-%}
-
-%%initializtion
-V = TR.Points;
-F = TR.ConnectivityList;
-vNum = size(V,1);
-
-bounList = cell2mat(boundary);
-bounLen = size(bounList,1);
-
-%%copy the loop boundary with a new set of vertex
-newV = zeros(vNum+bounLen,3);
-newV(1:vNum,:) = V(:,:);
-newV((vNum+1):(vNum+bounLen),:) = V(bounList(:),:);
-newF = F;
-
-for vertex = 1:bounLen
-    %%for each vertex, find the edge along it
-    centreVertex = bounList(vertex);
-    lastVertex = bounList(mod(vertex-1+bounLen-1,bounLen)+1);
-    nextVertex = bounList(mod(vertex-1+bounLen+1,bounLen)+1);
-
-    %%divide its neighbouring vertex into two sides
-    [~,side2] = divideVertex(TR,centreVertex,lastVertex,nextVertex);
-    side2List = cell2mat(side2);
-    side2Len = size(side2List,1);
-    
-    %%update all the faces of a side with the new vertex index
-    for neigh = 1:side2Len
-        neighFace = edgeAttachments(TR,centreVertex,side2List(neigh));
-        nfList = cell2mat(neighFace);
-        for face = 1:size(nfList,2)
-            [~,col] = find(F(nfList(face),:) == centreVertex,1,'first');
-            newF(nfList(face),col) = vNum+vertex;
-        end
-    end
-    if side2Len == 0
-        neighFace = edgeAttachments(TR,lastVertex,centreVertex);
-        nfList = cell2mat(neighFace);
-        for face = 1:size(nfList,2)
-            [~,colL] = find(F(nfList(face),:) == lastVertex,1,'first');
-            [~,colC] = find(F(nfList(face),:) == centreVertex,1,'first');
-            if (colC == 1 && colL == 2) || (colC == 2 && colL == 3) || (colC == 3 && colL == 1)
-                newF(nfList(face),colC) = vNum+vertex;
-            end
-        end
-    end
+wdge_list = [];
+for i=1:nv
+	v=vertex(i);
+	%construct wedge around v
+	[vr,vc] = find(he == v);
+	[vrc,vcc] = find(hec == v);
+	wdge = [];
+	if isempty(vr)
+		wdge = unique(vr);
+	else
+		
+	end
+	wdge_list = [wdge_list;wdge]
 end
 
-newTR = triangulation(newF,newV);
+vertex_s = unique(wdge_list);
+face_s = face;
+
+%%copy the loop boundary with a new set of vertex
+vertex_s = zeros(nvert+nbound,3);
+vertex_s(1:nvert,:) = vertex(:,:);
+vertex_s((nvert+1):(nvert+nbound),:) = vertex(boundary(:),:);
+face_s = face;
+
+[he,heif] = compute_halfedge(face);
+[am,amd] = compute_adjacency_matrix(face);
+
+
+for ivert = 1:nbound
+    adj_face = unique(heif(he(:,1)==boundary(ivert) | he(:,2)==boundary(ivert)))
+
+end
