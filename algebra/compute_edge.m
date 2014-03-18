a0790b61567ac3e9c385083ff522c462cc4f8b93
C Wen <cfwen@users.noreply.github.com>
2014-03-18 18:14:28 +0800

    add code for compute_face_ring and add doc for some

--- algebra/compute_edge.m
+++ algebra/compute_edge.m
@@ -1,3 +1,26 @@
+%% compute_edge 
+%  description of the function
+% 
+%  [detailed explanation]
+%
+%% Syntax
+%   [edge,eif] = compute_edge(face);
+%
+%% Description
+%  
+%
+%% Example
+%   [edge,eif] = compute_edge(face);
+%
+%% Contribution
+%  Author : Wen Chengfeng
+%  Created: 2014/03/
+%  Revised: 2014/03/18 by Wen, add doc
+% 
+%  Copyright 2014 Computational Geometry Group
+%  Department of Mathematics, CUHK
+%  http://www.lokminglui.com
+
 function [edge,eif] = compute_edge(face)
 [am,amd] = compute_adjacency_matrix(face);
 [I,J,~] = find(am);
