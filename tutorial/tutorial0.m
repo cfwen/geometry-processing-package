64a3a7c533b2bf42eebd77c0ea7e979c8ee8b940
C Wen <whywe@live.com>
2014-03-31 17:04:18 +0800

    minor revision and update tutorial

---
+++ tutorial/tutorial0.m
@@ -0,0 +1,62 @@
+%% tutorial
+% This tutorial will bring you go through the package, show you its design
+% principle and capbility.
+% 
+
+%% preparation
+% First we add the package folder to search path:
+% 
+%   addpath('geometric-processing-package')
+% 
+% startup will then add all subfolders in the packege to the top of search 
+% path and set output format
+startup
+
+%% read/write mesh
+% three mesh formats are supported: off, obj, ply
+[face,vertex] = read_off('data/face.off'); % read off format mesh
+sf = size(face) % face is a nf x 3 array
+sv = size(vertex) % vertex is a nv x 3 array
+write_obj('data/face.obj',face,vertex) % write mesh to obj format
+
+%% visualize mesh
+% we supply two visualizition functions: 
+% 
+%   plot_mesh(face,vertex)
+%   plot_path(face,vertex,path)
+% 
+% Both function uses pre-defined style to plot. You can easily specify your
+% own style. More details please read help doc.
+close all
+plot_mesh(face,vertex) % plot mesh
+axis off
+view(-90,-84)
+snapnow
+bd = compute_bd(face); % bd is the boundary
+plot_path(face,vertex,bd) % plot path (boundary) on mesh
+
+%%
+% Advanced plotting. You can specify your own style.
+figure('Position',[1 50 500 560],'Color',[1 1 1]); % set figure size and background color
+plot_mesh(face,vertex,...
+'EdgeColor',[20 20 20]/255,...
+'FaceColor',[251 175 147]/255,...
+'LineWidth',0.5,...
+'CDataMapping','scaled');
+axis equal
+axis tight
+view(-90,-84)
+axis off
+colormap hsv
+lighting phong
+light('Position',[-1 1 -1],'Style','infinite');
+%%
+% To export high quality images, we recommend an excellent library 
+% <http://www.mathworks.com/matlabcentral/fileexchange/23629-exportfig
+% export_fig>. Export above figure with export_fig 
+% 
+%   export_fig face -png -transparent -nocrop
+% 
+% <<face.png>>
+% 
+export_fig face -png -transparent -nocrop
---
+++ tutorial/tutorial1.m
@@ -0,0 +1,85 @@
+%% algebra and topology
+% This package provides various algebraic and topologic functions. Our goal
+% is to provide "engough" fundamental functions such that users can develop
+% their own applications based on (only) this package. Performance is
+% always in our mind when developing this package. To achieve best
+% performance, vectorized operation is employed whenever possible.
+% 
+% adjacency matrix is widely used. Nonzero element in am indicates a link
+% (edge) of the mesh. Likewise, nonzero element in amd indicates a link (halfedge)
+% of the mesh. Additionally, am(i,j) == 2 means that edge(i,j) is an
+% interior edge, while am(i,j) == 1 means edge(i,j) is an boundary edge,
+% since boundary edge appears only used by one face and interior edge 
+% shares by two faces.
+% Since halfedge is unique, i.e., every halfedge belongs to a single face, 
+% we store this connection between halfedge and face in amd. That is, 
+% amd(i,j) = k means that halfedge(i,j) belongs to face k, or vice verse,
+% face k has halfedge(i,j), or equivalently, face k has two vertex i and j
+% and they are in ccw order.
+[am,amd] = compute_adjacency_matrix(face);
+%%
+% Now we show how we can make use of adjacency matrix.
+
+% find edges of original mesh
+[I,J,V] = find(am);    % all nonzero elements in am
+ind = I<J;             % to avoid duplicity
+edge = [I(ind),J(ind)];% done!
+
+% find boundary edges
+ind = I<J & V == 1;
+% almost same with reslut of freeBoundary, except that bd2 is not in 
+% consective order and some edge's direction may be swapped
+bd2 = [I(ind),J(ind)];
+
+% plot boundary edges in surface
+figure
+plot_mesh(face,vertex)
+axis off
+view(-90,-84)
+hold on
+e1 = bd2(:,1); e2 = bd2(:,2); % start and end of edges
+plot3([vertex(e1,1),vertex(e2,1)]',[vertex(e1,2),vertex(e2,2)]',[vertex(e1,3),vertex(e2,3)]','r-','LineWidth',2)
+
+% to find boundary edges in consective order and right direction, we'd
+% better use amd, since it stores information of halfedge. Please read
+% function compute_bd for details.
+
+%%
+% Computation on mesh is uausllay decomposed to faces or vertices, for
+% example, calculate face area or vertex normal, discretize Laplace 
+% operator or gradeint operator. Such computation usually needs
+% neighbouring faces/vertices. 
+% 
+% There are two ways to do this. One natural way is first find neighbours 
+% then do the computation. Now we compute one-ring area at each vertex,
+% i.e., area of each vertex is the summation of area of its neighbour faces.
+
+% find compute one-ring faces of vertex
+[face,vertex] = read_off('bunny.off');
+fa = face_area(face,vertex);
+nv = size(vertex,1);
+%%
+
+% insteaf of using for loop to sum, we can use arrayfun, which is more efficient
+vfr = compute_vertex_face_ring(face);
+va = arrayfun(@(i) sum(fa(vfr{i})),1:nv);
+%%
+% We can also do it in a MUCH MORE efficent way. Since every halfedge is
+% attached with a face, and vise verse. And we can easily access halfedges
+% which start from a vertex. Then the one-ring vertex area can be
+% calculated in this way: for every halfedge, associate it with area of the 
+% face that the halfedge is attached to, then sum the area w.r.t. halfedge, 
+% which can be done by accumarray. Following is the code:
+fa = face_area(face,vertex);
+[he,heif] = compute_halfedge(face);
+va = accumarray(he(:,1),fa(heif));
+%%
+% Above two pieces of code do the same thing, however the second one is
+% much faster. We can use tic/toc command to record time. On my computer, 
+% first method uses 3.09s, while second method uses 0.044s, which shows
+% significant difference. It's not because we didn't optimize the code in
+% first method (in fact there is not much space to optimize), the reason is
+% that second method can be totally vectorized. Most time of first method
+% is spent on compute_vertex_face_ring (which replys on
+% compute_vertex_ring), it has to use loop to find neighbours of each
+% vertex. 
